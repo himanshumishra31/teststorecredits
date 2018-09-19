5a65e4e41547b2f2f5070ae43434db41aff67415
Himanshu <himanshumishra3@gmail.com>
2018-09-19 21:35:42 +0530

    mailer added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,16 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "mail.gmail.com",
+    authentication: :plain,
+    user_name: 'himanshumishra3@gmail.com' ,
+    password: 'chotumishra31' ,
+    enable_starttls_auto: true
+  }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
